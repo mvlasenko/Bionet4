46c38d4c0162e07d9ca06e4b6c8af17f89a73e56
Mark Vlasenko <mvlasenko@sdl.com>
2018-06-29 16:41:57 +0300

    fe layout started

---
+++ Bionet4/Helpers/MenuHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bionet4.Helpers
+{
+    public static class MenuHelper
+    {
+        public static HtmlString GetMainMenu(this HtmlHelper htmlHelper)
+        {
+
+        }
+
+    }
+}
