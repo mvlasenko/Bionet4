6a7ec8209df319b1e215155850320278db3c6fa8
Mark Vlasenko <mvlasenko@sdl.com>
2018-06-29 18:45:19 +0300

    sitemap added

--- Bionet4/Helpers/MenuHelper.cs
+++ Bionet4/Helpers/MenuHelper.cs
@@ -1,17 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using System.Text;
 using System.Web.Mvc;
+using MvcSiteMapProvider;
+using MvcSiteMapProvider.Web.Html;
 
 namespace Bionet4.Helpers
 {
     public static class MenuHelper
     {
-        public static HtmlString GetMainMenu(this HtmlHelper htmlHelper)
+        public static MvcHtmlString MainMenu(this MvcSiteMapHtmlHelper helper)
         {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<ul class=\"nav navbar-nav sf-menu\">");
 
+            foreach (ISiteMapNode item in helper.SiteMap.RootNode.ChildNodes)
+            {
+                ProcessItem(item, sb);
+            }
+
+            sb.Append("</ul>");
+
+            return new MvcHtmlString(sb.ToString());
         }
 
+        private static void ProcessItem(ISiteMapNode item, StringBuilder sb)
+        {
+            if (item.ChildNodes != null && item.ChildNodes.Count > 0)
+            {
+                sb.Append("<li>");
+
+                int level = item.GetNodeLevel();
+                if (level < 2)
+                {
+                    sb.AppendFormat("<a href=\"{0}\" class=\"sf-with-ul\">{1}<span class=\"sf-sub-indicator\"><i class=\"fa fa-angle-down\"></i></span></a>", item.Url, item.Title);
+                }
+                else
+                {
+                    sb.AppendFormat("<a href=\"{0}\" class=\"sf-with-ul\">{1}<span class=\"sf-sub-indicator pull-right\"><i class=\"fa fa-angle-right\"></i></span></a>", item.Url, item.Title);
+                }
+
+                sb.Append("<ul>");
+                foreach (ISiteMapNode childItem in item.ChildNodes)
+                {
+                    ProcessItem(childItem, sb);
+                }
+                sb.Append("</ul>");
+
+                sb.Append("</li>");
+            }
+            else
+            {
+                sb.AppendFormat("<li><a href=\"{0}\" class=\"sf-with-ul\">{1}</a></li>", item.Url, item.Title);
+            }
+        }
     }
 }
